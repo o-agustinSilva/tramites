7ef78b8eef27840d946c1ccb7636754de43bb38a
o-agustinSilva <agustinsilva1009@gmail.com>
2024-09-10 19:58:21 -0300

    Se ocultaron credenciales

--- tramites/tramites/settings.py
+++ tramites/tramites/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 USE_I18N = True
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
